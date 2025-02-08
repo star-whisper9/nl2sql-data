d7a04d54cacdc269475f14595f8020ad0ed823c8
starwhisper9 <star@sotis.space>
2025-02-08 15:13:45 +0800

    更新示例sql注释标注

--- sql/employees.sql
+++ sql/employees.sql
@@ -19,25 +19,26 @@ SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
 -- Table structure for departments
--- 部门名称可选值：Customer Service, Development, Finance, Human Resources, Marketing, Production, Quality Management, Research, Sales
+-- 部门表
 -- ----------------------------
 DROP TABLE IF EXISTS `departments`;
 CREATE TABLE `departments` (
-  `dept_no` char(4) NOT NULL,
-  `dept_name` varchar(40) NOT NULL,
+  `dept_no` char(4) NOT NULL, -- 部门编号列
+  `dept_name` varchar(40) NOT NULL, -- 部门名称列，可选值：Customer Service, Development, Finance, Human Resources, Marketing, Production, Quality Management, Research, Sales
   PRIMARY KEY (`dept_no`),
   UNIQUE KEY `dept_name` (`dept_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- ----------------------------
 -- Table structure for dept_emp
+-- 部门员工表
 -- ----------------------------
 DROP TABLE IF EXISTS `dept_emp`;
 CREATE TABLE `dept_emp` (
-  `emp_no` int NOT NULL,
-  `dept_no` char(4) NOT NULL,
-  `from_date` date NOT NULL,
-  `to_date` date NOT NULL,
+  `emp_no` int NOT NULL, -- 员工编号列
+  `dept_no` char(4) NOT NULL, -- 部门编号列
+  `from_date` date NOT NULL, -- 入职日期列
+  `to_date` date NOT NULL, -- 合同终止日期列
   PRIMARY KEY (`emp_no`,`dept_no`),
   KEY `dept_no` (`dept_no`),
   CONSTRAINT `dept_emp_ibfk_1` FOREIGN KEY (`emp_no`) REFERENCES `employees` (`emp_no`) ON DELETE CASCADE,
@@ -46,13 +47,14 @@ CREATE TABLE `dept_emp` (
 
 -- ----------------------------
 -- Table structure for dept_manager
+-- 部门经理表
 -- ----------------------------
 DROP TABLE IF EXISTS `dept_manager`;
 CREATE TABLE `dept_manager` (
-  `emp_no` int NOT NULL,
-  `dept_no` char(4) NOT NULL,
-  `from_date` date NOT NULL,
-  `to_date` date NOT NULL,
+  `emp_no` int NOT NULL, -- 员工编号列
+  `dept_no` char(4) NOT NULL, -- 部门编号列
+  `from_date` date NOT NULL, -- 任职日期列
+  `to_date` date NOT NULL, -- 离职日期列
   PRIMARY KEY (`emp_no`,`dept_no`),
   KEY `dept_no` (`dept_no`),
   CONSTRAINT `dept_manager_ibfk_1` FOREIGN KEY (`emp_no`) REFERENCES `employees` (`emp_no`) ON DELETE CASCADE,
@@ -61,40 +63,43 @@ CREATE TABLE `dept_manager` (
 
 -- ----------------------------
 -- Table structure for employees
+-- 员工表
 -- ----------------------------
 DROP TABLE IF EXISTS `employees`;
 CREATE TABLE `employees` (
-  `emp_no` int NOT NULL,
-  `birth_date` date NOT NULL,
-  `first_name` varchar(14) NOT NULL,
-  `last_name` varchar(16) NOT NULL,
-  `gender` enum('M','F') NOT NULL,
-  `hire_date` date NOT NULL,
+  `emp_no` int NOT NULL, -- 员工编号列
+  `birth_date` date NOT NULL, -- 出生日期列
+  `first_name` varchar(14) NOT NULL, -- 名列
+  `last_name` varchar(16) NOT NULL, -- 姓列
+  `gender` enum('M','F') NOT NULL, -- 性别列
+  `hire_date` date NOT NULL, -- 入职日期列
   PRIMARY KEY (`emp_no`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- ----------------------------
 -- Table structure for salaries
+-- 薪资记录表
 -- ----------------------------
 DROP TABLE IF EXISTS `salaries`;
 CREATE TABLE `salaries` (
-  `emp_no` int NOT NULL,
-  `salary` int NOT NULL,
-  `from_date` date NOT NULL,
-  `to_date` date NOT NULL,
+  `emp_no` int NOT NULL, -- 员工编号列
+  `salary` int NOT NULL, -- 薪资列
+  `from_date` date NOT NULL, -- 薪资周期起始日期列
+  `to_date` date NOT NULL, -- 薪资周期结束日期列
   PRIMARY KEY (`emp_no`,`from_date`),
   CONSTRAINT `salaries_ibfk_1` FOREIGN KEY (`emp_no`) REFERENCES `employees` (`emp_no`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- ----------------------------
 -- Table structure for titles
+-- 职称表
 -- ----------------------------
 DROP TABLE IF EXISTS `titles`;
 CREATE TABLE `titles` (
-  `emp_no` int NOT NULL,
-  `title` varchar(50) NOT NULL,
-  `from_date` date NOT NULL,
-  `to_date` date DEFAULT NULL,
+  `emp_no` int NOT NULL, -- 员工编号列
+  `title` varchar(50) NOT NULL, -- 职称列
+  `from_date` date NOT NULL, -- 职称生效日期列
+  `to_date` date DEFAULT NULL, -- 职称失效日期列
   PRIMARY KEY (`emp_no`,`title`,`from_date`),
   CONSTRAINT `titles_ibfk_1` FOREIGN KEY (`emp_no`) REFERENCES `employees` (`emp_no`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
